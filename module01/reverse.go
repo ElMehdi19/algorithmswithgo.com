2247e098880879798ea7bdadb413427e4f9c886a
El Mehdi Rami <elmehdirami5@gmail.com>
2021-01-09 05:10:36 +0100

    reverse done

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,33 @@
 package module01
 
+import "strings"
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
+
+func pop(stack *[]string) string {
+	outputRef := *stack
+	lastItem := string(outputRef[len(outputRef) - 1])
+	*stack = outputRef[0:len(outputRef) - 1]
+	return lastItem
+}
+
 func Reverse(word string) string {
-	return ""
+	stack := make([]string, len(word))
+	for i, char := range word {
+		stack[i] = string(char)
+	}
+
+	var item string
+	reversed := make([]string, len(word))
+	for len(stack) != 0 {
+		item = pop(&stack)
+		reversed = append(reversed, item)
+	}
+
+	output := strings.Join(reversed, "")
+	return output
 }
